2328135b343db72e1ad87c756d2689037e2372a2
asadisaghar <asadisaghar@gmail.com>
2018-09-21 09:58:50 +0200

    tested-WORKING-writes on the local machine

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
-#! /bin/sh
+# #! /bin/sh
 
-export DOCKER_HOST=tcp://ymslanda.innovationgarage.tech:2375
+# export DOCKER_HOST=tcp://ymslanda.innovationgarage.tech:2375
 
-docker build --tag ymslanda.innovationgarage.tech:5000/portpollution_aisreduction:latest .
-docker push ymslanda.innovationgarage.tech:5000/portpollution_aisreduction:latest
-docker stack deploy -c docker-compose.yml portpollution_aisreduction
+# docker build --tag ymslanda.innovationgarage.tech:5000/portpollution_aisreduction:latest .
+# docker push ymslanda.innovationgarage.tech:5000/portpollution_aisreduction:latest
+# docker stack deploy -c docker-compose.yml portpollution_aisreduction
+
+DATA="."
+spark-submit --master spark://ymslanda.innovationgarage.tech:7077 easyPi.py 100 $DATA/pi.res
